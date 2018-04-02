711f7f464820e218a65c58ccfd58fa07f6e14a23
Mariko <marikoanderson@gmail.com>
2018-04-02 16:37:37 -0700

    created conroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'rides/index'
+
+  get 'rides/show'
+
+  get 'rides/new'
+
+  get 'rides/create'
+
+  get 'rides/edit'
+
+  get 'rides/update'
+
+  get 'rides/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
