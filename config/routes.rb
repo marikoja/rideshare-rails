cc8ee487082b7dbecc138036725c1e38e3e0d521
Mariko <marikoanderson@gmail.com>
2018-04-02 17:15:03 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'rides/index'
+  root 'trips#index' #defines homepage
 
-  get 'rides/show'
-
-  get 'rides/new'
-
-  get 'rides/create'
-
-  get 'rides/edit'
-
-  get 'rides/update'
-
-  get 'rides/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/trips' , to: 'trips#index', as: 'trips'
+  get '/trips/new', to: 'trips#new', as: 'new_trip'
+  post '/trips', to:'trips#create'
+  get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
+  get '/trips/:id', to: 'trips#show', as: 'trip'
+  patch '/trips/:id', to: 'trips#update'
+  delete '/trips/:id', to: 'trips#destroy'
 end
