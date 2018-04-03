5457e30a6da33af332ed749d4a5cb97805a6da89
Mariko <marikoanderson@gmail.com>
2018-04-02 17:48:44 -0700

    added methods new, create and edit

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,12 +6,20 @@ class RidesController < ApplicationController
   end
 
   def new
+    @trip = Trip.new
   end
 
   def create
+    trip = Trip.new(trip_params)
+    if trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id])
   end
 
   def update
@@ -19,4 +27,5 @@ class RidesController < ApplicationController
 
   def destroy
   end
+
 end
