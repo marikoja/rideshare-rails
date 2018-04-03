ee4bd4a0902f18afcb16ce3d86b9f36960df393e
Aruna <aruna.thungathurthi@gmail.com>
2018-04-02 17:50:10 -0700

    added index and show methods

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,11 +1,15 @@
 class RidesController < ApplicationController
   def index
+    @trip = Trip.all
   end
 
   def show
+    id = params[:id]
+    @trip = Trip.find(id)
   end
 
   def new
+
   end
 
   def create
@@ -15,6 +19,16 @@ class RidesController < ApplicationController
   end
 
   def update
+    @trip = Trip.find_by(id:params[:id])
+    if !@trip.nil?
+      if @trip.update(trip_params)
+        redirect_to trip_path(@trip.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to trips_path
+    end
   end
 
   def destroy
