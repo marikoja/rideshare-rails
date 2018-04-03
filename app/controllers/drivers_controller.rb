f80cb9e53e004e52244a882bc8c3359d50f3a349
Mariko <marikoanderson@gmail.com>
2018-04-03 14:06:33 -0700

    added driver routes and index

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,22 +1,56 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all
   end
 
   def show
+    id = params[:id]
+    @driver = Driver.find(id)
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
+    driver = Driver.new(driver_params)
+    if driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @driver = Driver.find_by(id: params[:id])
   end
 
   def update
+    @driver = Trip.find_by(id: params[:id])
+    if !@driver.nil?
+      if @driver.update(driver_params)
+        redirect_to driver_path(@driver.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to trips_path
+    end
   end
 
   def destroy
+    id = params[:id]
+    @driver = Driver.find(id)
+    if @driver
+      @driver.destroy
+    end
+    redirect_to drivers_path
+  end
+
+private
+
+  def driver_params
+    return params.require(:driver).permit(:driver_id)
+
   end
 end
