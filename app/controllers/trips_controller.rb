eb32115ffd7d08b2b686d601afbf39b4a495cbae
Aruna <aruna.thungathurthi@gmail.com>
2018-04-04 15:07:13 -0700

    updated create method for adding new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -13,45 +13,56 @@ class TripsController < ApplicationController
   end
 
   def create
-    trip = Trip.new(passenger: params[:passenger_id], driver:random_driver,
-      cost:rand(100..15000), rating: 0, date: Date.today)
+    if params[:passenger_id]
+      # This is the nested route, /author/:author_id/books/new
+      passenger = Passenger.find_by(id: passenger_id)
+      driver = Driver.random_driver
+      cost = rand(10..500)
+      rating = 0
+      @trips = passenger.trip.new
 
-    if trip.save
-      redirect_to trips_path
     else
-      render :new
+      # This is the 'regular' route, /books/new
+      # @trips = trip.new
     end
-  end
 
-  def edit
-    @trip = Trip.find_by(id: params[:id])
+  trip = Trip.new
+  if trip.save
+    redirect_to trips_path
+  else
+    render :new
   end
+end
 
-  def update
-    @trip = Trip.find_by(id:params[:id])
-    if !@trip.nil?
-      if @trip.update(trip_params)
-        redirect_to trip_path(@trip.id)
-      else
-        render :edit
-      end
+def edit
+  @trip = Trip.find_by(id: params[:id])
+end
+
+def update
+  @trip = Trip.find_by(id:params[:id])
+  if !@trip.nil?
+    if @trip.update(trip_params)
+      redirect_to trip_path(@trip.id)
     else
-      redirect_to trips_path
+      render :edit
     end
-  end
-
-  def destroy
-    id = params[:id]
-    @trip = Trip.find(id)
-    if @trip
-      @trip.destroy
-    end
+  else
     redirect_to trips_path
   end
+end
+
+def destroy
+  id = params[:id]
+  @trip = Trip.find(id)
+  if @trip
+    @trip.destroy
+  end
+  redirect_to trips_path
+end
 
 private
-  def trip_params
-    return params.require(:trip).permit(:trip_id)
-  end
+def trip_params
+  return params.require(:trip).permit(:trip_id)
+end
 
 end
